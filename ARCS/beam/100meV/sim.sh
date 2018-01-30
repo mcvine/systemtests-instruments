4dab43dbe109c5056ff72ab80fc3451970528faa
Jiao Lin <linjiao@ornl.gov>
2018-01-30 08:42:55 -0500

    ARCS/beam/100meV/sim.sh: change to ncount=1e7

--- ARCS/beam/100meV/sim.sh
+++ ARCS/beam/100meV/sim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -e
-mcvine instruments arcs beam -E=100 -ncount=1e4 -nodes=4
+mcvine instruments arcs beam -E=100 -ncount=1e7 -nodes=4
 rm -rf _m2sout
-mkdir -p ~/beam/ARCS/100meV-n1e4
-rm -rf ~/beam/ARCS/100meV-n1e4/*
-mv * ~/beam/ARCS/100meV-n1e4/
+mkdir -p ~/beam/ARCS/100meV-n1e7
+rm -rf ~/beam/ARCS/100meV-n1e7/*
+mv * ~/beam/ARCS/100meV-n1e7/
