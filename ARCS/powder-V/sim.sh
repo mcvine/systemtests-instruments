ecff86006d3a281564b239229ab6c36dcc73063c
Jiao Lin <linjiao@ornl.gov>
2018-02-01 08:45:28 -0500

    ARCS/powder-V/sim.sh: lower ncount so it can finish quickly

--- ARCS/powder-V/sim.sh
+++ ARCS/powder-V/sim.sh
@@ -4,7 +4,7 @@ mcvine workflow powder --instrument=ARCS --sample=V --workdir=mysim
 cd mysim
 rm -rf beam
 ln -s ~/beam/ARCS/100meV beam
-make NCOUNT=1e7 NODES=4
+make NCOUNT=1e5 NODES=4
 aws s3 cp --profile ${AWS_S3_PROFILE_NAME} *.nxs \
     s3://ndav-mcvine/systemtests-instruments/${TRAVIS_JOB_NUMBER}/ARCS/powder-V/
 cd -
