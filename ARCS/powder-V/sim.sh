5e9fb03008aaa689c9f5a462792aa242715beee2
Jiao Lin <linjiao@ornl.gov>
2018-02-01 09:40:47 -0500

    debugging: why reduce failed?

--- ARCS/powder-V/sim.sh
+++ ARCS/powder-V/sim.sh
@@ -5,6 +5,7 @@ cd mysim
 rm -rf beam
 ln -s ~/beam/ARCS/100meV beam
 make NCOUNT=1e5 NODES=4
+cat log.reduce
 aws s3 cp --profile ${AWS_S3_PROFILE_NAME} *.nxs \
     s3://ndav-mcvine/systemtests-instruments/${TRAVIS_JOB_NUMBER}/ARCS/powder-V/
 cd -
