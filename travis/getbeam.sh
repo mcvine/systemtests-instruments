72a30b6c0c6c00d135f926eb3c46194dcc86366c
Jiao Lin <linjiao@ornl.gov>
2018-02-01 11:21:37 -0500

    travis/getbeam.sh: always setup aws

--- travis/getbeam.sh
+++ travis/getbeam.sh
@@ -4,8 +4,9 @@ set -e
 
 echo "Get beam"
 
+source activate testenv
+python travis/setup-aws-testconfig.py
+
 if [ ${USE_CACHE} == "no" ] ; then
-    source activate testenv
-    python travis/setup-aws-testconfig.py
     aws s3 sync s3://ndav-mcvine/beam/ $HOME/beam/ --profile ${AWS_S3_PROFILE_NAME}
 fi
