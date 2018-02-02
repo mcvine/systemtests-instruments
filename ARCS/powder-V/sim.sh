e5a1ab80053b59629673c4f7b266afeba49dbdd6
Jiao Lin <linjiao@ornl.gov>
2018-02-01 21:17:54 -0500

    arcs powder-V: adding validation

--- ARCS/powder-V/sim.sh
+++ ARCS/powder-V/sim.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+conda list mcvine
+
 mcvine workflow powder --instrument=ARCS --sample=V --workdir=mysim
 cd mysim
 rm -rf beam
 ln -s ~/beam/ARCS/100meV beam
-make NCOUNT=1e5 NODES=4
+make NCOUNT=$NCOUNT NODES=$NODES BUFFER_SIZE=$BUFFER_SIZE
 cat log.scatter
 cat log.create-nxs
 cat log.reduce
 ls
-aws s3 cp \
-    iqe.nxs s3://ndav-mcvine/systemtests-instruments/${TRAVIS_JOB_NUMBER}/ARCS/powder-V/iqe.nxs \
-    --profile ${AWS_S3_PROFILE_NAME} 
+
 cd -
+python post-process.py
+
+# plot and save to S3
+plothist iqe.h5 --min=0 --output=iqe.png
+plothist ie.h5 --output=ie.png
+S3_DEST=s3://ndav-mcvine/systemtests-instruments/${TRAVIS_JOB_NUMBER}/ARCS/powder-V
+S3_OPTS="--profile ${AWS_S3_PROFILE_NAME}"
+# aws s3 cp iqe.png ${S3_DEST}/iqe.png ${S3_OPTS}
+# aws s3 cp ie.png ${S3_DEST}/ie.png ${S3_OPTS}
+
+# validate result
+python validate.py
--- ARCS/powder-V/test_arcs_powder_V.py
+++ ARCS/powder-V/test_arcs_powder_V.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python
+
 import os
 here = os.path.dirname(__file__)
 os.chdir(here)
 
 def test():
-    if os.system("bash sim.sh"):
+    if os.system("NCOUNT=4e5 NODES=4 BUFFER_SIZE=100000 ./sim.sh"):
         raise RuntimeError("Failed")
     return
+
+if __name__ == '__main__': test()
