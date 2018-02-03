7af1884485c17570b39cf02c646a6167e4ac0566
Jiao Lin <linjiao@ornl.gov>
2018-02-02 21:45:53 -0500

    fixing s3 upload

--- ARCS/powder-V/sim.sh
+++ ARCS/powder-V/sim.sh
@@ -18,9 +18,11 @@ python post-process.py
 # plot and save to S3
 plothist iqe.h5 --min=0 --output=iqe.png
 plothist ie.h5 --output=ie.png
-S3_DEST=s3://ndav-mcvine/systemtests-instruments/${TRAVIS_JOB_NUMBER}/ARCS/powder-V
+S3_DEST=s3://ndav-mcvine/systemtests-instruments/${BUILD_ID}/${BUILD_NUMBER}/ARCS/powder-V
 S3_OPTS="--profile ${AWS_S3_PROFILE_NAME}"
-aws s3 cp iqe.png ${S3_DEST}/iqe.png ${S3_OPTS}
+CMD="aws s3 cp iqe.png ${S3_DEST}/iqe.png ${S3_OPTS}"
+echo $CMD
+$CMD
 aws s3 cp ie.png ${S3_DEST}/ie.png ${S3_OPTS}
 
 # validate result
--- jenkins/run-tests.sh
+++ jenkins/run-tests.sh
@@ -9,5 +9,5 @@ source activate testenv
 
 conda install -c mcvine/label/unstable mcvine mcvine.workflow mcvine.phonon
 
-AWS_S3_PROFILE_NAME=ndav_mcvine
+export AWS_S3_PROFILE_NAME=ndav_mcvine
 py.test -s
