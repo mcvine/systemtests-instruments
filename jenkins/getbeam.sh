7a7990cc0f13ed4c86040e2b4ecb3cb821043342
Jiao Lin <linjiao@ornl.gov>
2018-02-04 13:47:53 -0500

    jenkins/getbeam.sh: need s3 profile name

--- jenkins/getbeam.sh
+++ jenkins/getbeam.sh
@@ -7,4 +7,5 @@ echo "Get beam"
 export PATH=$HOME/mc/bin:$PATH
 source activate testenv
 
+export AWS_S3_PROFILE_NAME=ndav_mcvine
 aws s3 sync s3://ndav-mcvine/beam/ $HOME/beam/ --profile ${AWS_S3_PROFILE_NAME}
