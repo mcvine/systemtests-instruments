366f0805f3777507232dc609ab90786c6321213f
Jiao Lin <linjiao@ornl.gov>
2018-02-04 13:43:26 -0500

    debugging

--- SEQ/powder-V/test_seq_powder_V.py
+++ SEQ/powder-V/test_seq_powder_V.py
@@ -5,7 +5,6 @@ import os, subprocess as sp, shlex
 def test():
     here = os.path.dirname(__file__)
     print here
-    return
     os.chdir(here)
     cmd = 'NCOUNT=8e5 NODES=8 BUFFER_SIZE=100000 QAXIS="0 8 0.05" ./sim.sh'
     if os.system(cmd):
