45e4c73b06ff830ecba94a4bf9c35f996b75b890
Jiao Lin <linjiao@ornl.gov>
2018-02-10 19:14:37 -0500

    jenkins/run-tests.sh: use metapackage "mcvine-full"

--- jenkins/run-tests.sh
+++ jenkins/run-tests.sh
@@ -7,7 +7,7 @@ echo "System testing of instruments"
 export PATH=$HOME/mc/bin:$PATH
 source activate testenv
 
-conda install -c mcvine/label/unstable mcvine mcvine.workflow mcvine.phonon mcvine.instruments
+conda install -c mcvine/label/unstable mcvine-full
 
 export AWS_S3_PROFILE_NAME=ndav_mcvine
 py.test -s
