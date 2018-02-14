ddf0867ffcd285da30e641bfa5effc18b56f251f
Jiao Lin <linjiao@ornl.gov>
2018-02-14 16:39:17 -0500

    Refs mcvine/mcvine#269. test new versions of mcvine.instruments and mcvine.workflow

--- jenkins/run-tests.sh
+++ jenkins/run-tests.sh
@@ -11,8 +11,7 @@ rm -rf $HOME/mc/pkgs/mcvine*
 
 set -e
 conda config --add channels mantid  # need mantid-framework
-# conda install -c mcvine/label/unstable mcvine
-conda install -c mcvine/label/exp mcvine
+conda install -c mcvine/label/unstable mcvine
 
 export AWS_S3_PROFILE_NAME=ndav_mcvine
 py.test -s
