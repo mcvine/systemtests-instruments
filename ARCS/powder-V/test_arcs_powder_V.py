20c8de8fa1244eea6c82fa55f4d021adefebf6f7
Jiao Lin <linjiao@ornl.gov>
2018-05-17 20:34:38 -0400

    Refs #1. added powder-V/test_arcs_powder_V.py

---
+++ ARCS/powder-V/test_arcs_powder_V.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os, subprocess as sp, shlex
+
+def test():
+    here = os.path.dirname(__file__) or '.'
+    print here
+    os.chdir(here)
+    cmd = "bash run.sh"
+    if os.system(cmd):
+        raise RuntimeError("Failed")
+    return
+
+if __name__ == '__main__': test()
