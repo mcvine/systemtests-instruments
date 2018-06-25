a5fd00c691be3361e90cbc43a71aeca5bf77d9b8
Jiao Lin <linjiao@ornl.gov>
2018-06-25 10:07:59 -0400

    powder-V/test_hyspec_powder_V.py: reduce ncount to reduce time

--- HYSPEC/powder-V/test_hyspec_powder_V.py
+++ HYSPEC/powder-V/test_hyspec_powder_V.py
@@ -6,7 +6,7 @@ def test():
     here = os.path.dirname(__file__) or '.'
     print here
     os.chdir(here)
-    cmd = 'NCOUNT=8e7 NODES=8 BUFFER_SIZE=1000000 QAXIS="0 8 0.05" DETECTOR_VESSEL_ANGLE=0 ./sim.sh'
+    cmd = 'NCOUNT=8e5 NODES=8 BUFFER_SIZE=1000000 QAXIS="0 8 0.05" DETECTOR_VESSEL_ANGLE=0 ./sim.sh'
     if os.system(cmd):
         raise RuntimeError("Failed")
     return
