a4daddcfe5bc7ec77c18171a38e9b05ba5156f99
Jiao Lin <jiao.lin@gmail.com>
2020-09-27 09:16:09 -0400

    debugging aws codebuild

--- aws-codebuild/install-conda.sh
+++ aws-codebuild/install-conda.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -x
+set -e
 cd $HOME
 # conda
 wget --quiet http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
--- aws-codebuild/test.sh
+++ aws-codebuild/test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 set -x
+set -e
 export PATH=$HOME/mc/bin:$PATH
 source activate test
 
+# aws configuration?
 export AWS_S3_PROFILE_NAME=ndav_mcvine
 
 # checking installation
