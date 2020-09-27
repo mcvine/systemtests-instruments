676e10bd896f0a6e16b30c9270614e6d26416257
Jiao Lin <jiao.lin@gmail.com>
2020-09-27 19:49:36 -0400

    testutils.execute: add env vars as input; print as cmd goes

--- testutils.py
+++ testutils.py
@@ -1,13 +1,22 @@
-import sys, subprocess as sp, psutil, shlex
-def execute(cmd):
+from __future__ import print_function
+import os, sys, subprocess as sp, psutil, shlex
+def execute(cmd, env=None, print_output=True):
+    extra = env or dict()
+    env = os.environ; env.update(extra)
     args = shlex.split(cmd)
     # with psutil.Popen(args, shell=True, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT, close_fds=True) as process:
-    with psutil.Popen(args, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT) as process:
-        process.wait()
-        output = process.stdout.read()
-        errcode = process.returncode
-    if sys.version_info >= (3,0):
-        output = output.decode()
+    with psutil.Popen(args, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT, env=env) as process:
+        output = ''
+        while True:
+            line = process.stdout.readline()
+            if sys.version_info >= (3,0):
+                line = line.decode()
+            output+=line
+            if line == '' and process.poll() is not None:
+                break
+            if line and print_output:
+                print(line.strip())
+        errcode = process.poll()
     if errcode:
         raise RuntimeError("{} failed:\nOutput:\n{}".format(cmd, output))
     return
