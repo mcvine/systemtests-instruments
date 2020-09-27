31a514b15370dca45bcd3c55f19b6c5d79ac118e
Jiao Lin <jiao.lin@gmail.com>
2020-09-27 11:24:51 -0400

    aws codebuild: fix path of calling getbeam.sh

--- aws-codebuild/getbeam.sh
+++ aws-codebuild/getbeam.sh
@@ -7,7 +7,8 @@ echo "Get beam"
 export PATH=$HOME/mc/bin:$PATH
 source activate testenv
 
-python ./setup-aws-testconfig.py
+THIS_SCRIPT_DIR=`dirname $0`
+python $THIS_SCRIPT_DIR/setup-aws-testconfig.py
 
 # export AWS_S3_PROFILE_NAME=ndav_mcvine
 aws s3 sync s3://ndav-mcvine/beam/ $HOME/beam/ --profile ${AWS_S3_PROFILE_NAME}
--- aws-codebuild/test.sh
+++ aws-codebuild/test.sh
@@ -6,7 +6,8 @@ export PATH=$HOME/mc/bin:$PATH
 source activate test
 
 # export AWS_S3_PROFILE_NAME=ndav_mcvine
-./getbeam.sh
+THIS_SCRIPT_DIR=`dirname $0`
+$THIS_SCRIPT_DIR/getbeam.sh
 
 # checking installation
 which py.test
