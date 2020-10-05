573e891a06ba31dc2cf4e13c1c1447ab9eb246c5
Jiao Lin <jiao.lin@gmail.com>
2020-10-05 11:44:33 -0400

    use conda create to install all:   aws-codebuild/install-mcvine.sh

--- aws-codebuild/install-mcvine.sh
+++ aws-codebuild/install-mcvine.sh
@@ -3,9 +3,8 @@
 set -x
 set -e
 export PATH=$HOME/mc/bin:$PATH
-conda create -n test python=$PYTHON_VERSION
+conda create -n test -c mcvine/label/unstable mcvine=1.4 pytest awscli python=$PYTHON_VERSION
 source activate test
-conda install -c mcvine/label/unstable mcvine pytest awscli
 conda list mcvine
 conda list mcvine-core
 THIS_SCRIPT_DIR=`dirname $0`
